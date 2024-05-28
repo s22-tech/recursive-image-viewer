c45a5c0cb7deeea0715d4d13a9fefe632bba4292
s22 Tech <59073912+s22-tech@users.noreply.github.com>
2024-05-27 18:49:48 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,64 +1,3 @@
-<?php
-
-/**
- * Recursively scans a directory and returns an array of the images within
- *
- * This can be used as a stand alone application for viewing images or if you
- * are a web developer you can put this in the root of your site and easily
- * ahve a way to search all the images on your site from one page.
- *
- * @global array $types
- *     The image types to look for
- * @param string $dir
- *     The directory to scan
- * @param array $images
- *     An array to hold the images
- *
- * @return array
- *     The Images
- */
-function get_images_recursive($dir, $images = array()) {
-  global $types;
-
-  // filetypes to display
-  $types = array("jpeg", "jpg", "gif", "png");
-
-  // add trailing slash if missing
-  if (substr($dir, -1) != "/") {
-    $dir .= "/";
-  }
-
-  $d = dir($dir) or die("get_images_recursive: Failed opening directory $dir for reading");
-
-  while (false !== ($item = $d->read())) {
-    // skip hidden files
-    if ($item[0] == ".") {
-      continue;
-    }
-
-    if (is_dir($dir . $item) && isset($_GET['all'])) {
-      $this_dir = get_images_recursive($dir . $item);
-      $images   = array_merge($images, $this_dir);
-    }
-
-    // check for image files
-    $path     = $dir . $item;
-    $mimetype = pathinfo($path, PATHINFO_EXTENSION);
-
-    if (in_array(strtolower($mimetype), $types)) {
-      $images[] = array(
-        'path' => $path,
-        'file' => "/" . $dir . $item,
-        'size' => getimagesize($dir . $item)
-      );
-    }
-  }
-  $d->close();
-
-  return $images;
-
-}
-?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -94,82 +33,151 @@ function get_images_recursive($dir, $images = array()) {
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
-  </head>
-  <body>
+	</head>
+	<body>
     <div class="container">
-      <?php
-      $output = NULL;
-      if (!empty($_GET['dir'])) {
+<?php
+	$output = null;
+	if (!empty($_GET['dir'])) {
+		$dir = $_GET['dir'];
+	  // Add trailing slash if missing.
+		if (!str_ends_with($dir, '/')) {
+			$dir .= '/';
+		}
 
-        $dir = $_GET['dir'];
-        // add trailing slash if missing
-        if (substr($dir, -1) != "/") {
-          $dir .= "/";
-        }
+		$d = dir($dir) or die("get_images_recursive 1: Failed opening directory $dir for reading");
+		$output .= '<ul>';
+		while (($item = $d->read()) !== false) {
+		  // Skip hidden files.
+			if ($item[0] == '.') {
+				continue;
+			}
+			if (is_dir($dir . $item)) {
+				$output .= '<li><a href="?dir='.$dir . $item .'"> View images in /'. $dir . $item .'</a></li>';
+			}
+		}
+		$output .= '</ul>';
 
-        $d    = dir($dir) or die("get_images_recursive: Failed opening directory $dir for reading");
-        $output .= "<ul>";
-        while (false !== ($item = $d->read())) {
-          // skip hidden files
-          if ($item[0] == ".") {
-            continue;
-          }
-          if (is_dir($dir . $item)) {
-            $output .= "<li><a href=\"?dir=$dir$item\">images from /$dir$item</a></li>";
-          }
-        }
-        $output .= "</ul>";
+	  // Fetch image details.
+		$images = get_images_recursive($dir);
 
-        // fetch image details
-        $images = get_images_recursive($dir);
+		$ratio = 0.3;
+		foreach ($images as $img) {
+			$width  = $img['size'][0] * $ratio;
+			$height = $img['size'][1] * $ratio;
 
-        $ratio = 0.3;
-        foreach ($images as $img) {
-          $width  = $img['size'][0] * $ratio;
-          $height = $img['size'][1] * $ratio;
+			// If you want to set the width and height in code, delete the
+			// img class above in the CSS and add the following line to the image tag:
+			// width=\"{$width}\" height=\"{$height}\"
 
-          // If you want to set the width and height in code delete the
-          // img class above in the CSS and add the following line to the image tag
-          // width=\"{$width}\" height=\"{$height}\"
+			$output .= '<div class="photo">';
+			$output .= "<a target=\"_blank\" href=\"{$img['path']}\">";
+			$output .= '<img class="lazy" data-original="'.$img['path'].'" alt=""></a><br>' . PHP_EOL;
+			
+		  // Display image file name as link.
+			$output .= '<a target="_blank" href="'.$img['path'].'">' . basename($img['file']) . '</a><br>' . PHP_EOL;
+			
+		  // Display image dimenstions.
+			$output .= "({$img['size'][0]} x {$img['size'][1]} pixels)<br>" . PHP_EOL;
+			
+		  // Display mime_type.
+			$output .= $img['size']['mime'];
+			$output .= '</div>' . PHP_EOL;
+		}
+	}
+  // Link to the directories.
+	else {
+		$dir ?? '';
+		$d = dir(__DIR__ . '/' . $dir) or die('get_images_recursive 2: Failed opening directory ' . __DIR__ . '/' . $dir . ' for reading');
+		$output .= '<ul>';
+		while (($item = $d->read()) !== false) {
+		  // Skip hidden files.
+			if ($item[0] == '.') {
+				continue;
+			}
+			if (is_dir($dir . $item)) {
+				$output .= '<li><a href="?dir='. $item .'"> View images in the '. $item .' folder.</a></li>';
+			}
+		}
+		$output .= '</ul>';
+	}
 
-          $output .= "<div class=\"photo\">";
-          $output .= "<a target=\"_blank\" href=\"{$img['path']}\">";
-          $output .= "<img class=\"lazy\" data-original=\"{$img['path']}\" alt=\"\"></a><br>\n";
-          // display image file name as link
-          $output .= "<a target=\"_blank\" href=\"{$img['path']}\">" . basename($img['file']) . "</a><br>\n";
-          // display image dimenstions
-          $output .= "({$img['size'][0]} x {$img['size'][1]} pixels)<br>\n";
-          // display mime_type
-          $output .= $img['size']['mime'];
-          $output .= "</div>\n";
-        }
-      }
-      // Link to the directories
-      else {
-        $d    = dir(__DIR__ . '/' . $dir) or die("get_images_recursive: Failed opening directory " . __DIR__ . '/' . $dir . " for reading");
-        $output .= "<ul>";
-        while (false !== ($item = $d->read())) {
-          // skip hidden files
-          if ($item[0] == ".") {
-            continue;
-          }
-          if (is_dir($dir . $item)) {
-            $output .= "<li><a href=\"?dir=$item\">images from /$item</a></li>";
-          }
-        }
-        $output .= "</ul>";
-      }
-
-      $output = str_replace('<ul></ul>', '', $output);
-      if (!empty($output)) {
-        print $output;
-      }
-      ?>
+	$output = str_replace('<ul></ul>', '', $output);
+	if (!empty($output)) {
+		print $output;
+}
+?>
     </div>
-    <script type="text/javascript" charset="utf-8">
+    <script charset="utf-8">
       $(function () {
-        $("img.lazy").lazyload();
+        $('img.lazy').lazyload();
       });
     </script>
   </body>
 </html>
+
+<?php
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
+	/**
+	 * https://github.com/tomgould/recursive-image-viewer
+	 *
+	 * Recursively scans a directory and returns an array of the images within.
+	 *
+	 * This can be used as a stand alone application for viewing images or if you
+	 * are a web developer you can put this in the root of your site and easily
+	 * have a way to search all the images on your site from one page.
+	 *
+	 * @global array $types
+	 *     The image types to look for
+	 * @param string $dir
+	 *     The directory to scan
+	 * @param array $images
+	 *     An array to hold the images
+	 *
+	 * @return array
+	 *     The Images
+	 */
+	function get_images_recursive($dir, $images = []) {
+		global $types;
+
+	  // Filetypes to display.
+		$types = ['jpeg', 'jpg', 'gif', 'png'];
+
+	  // Add trailing slash if missing.
+		if (!str_ends_with($dir, '/')) {
+			$dir .= '/';
+		}
+
+		$d = dir($dir) or die("get_images_recursive: Failed opening directory $dir for reading");
+
+		while (($item = $d->read()) !== false) {
+		  // Skip hidden files.
+			if ($item[0] == '.') {
+				continue;
+			}
+
+			if (is_dir($dir . $item) && isset($_GET['all'])) {
+				$this_dir = get_images_recursive($dir . $item);
+				$images   = array_merge($images, $this_dir);
+			}
+
+		  // Check for image files.
+			$path     = $dir . $item;
+			$mimetype = pathinfo($path, PATHINFO_EXTENSION);
+
+			if (in_array(strtolower($mimetype), $types)) {
+				$images[] = [
+					'path' => $path,
+					'file' => '/' . $dir . $item,
+					'size' => getimagesize($dir . $item)
+				];
+			}
+		}
+		$d->close();
+
+		return $images;
+	}
+?>
